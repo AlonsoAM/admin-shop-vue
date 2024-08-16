f3eddf158a6f635b6406dd9832ad6f1a4dfc92a8
Alfredo Alonso Anchante Moreno <alonso.amoreno@live.com>
2024-08-16 18:56:00 -0500

    Estructura de carpetas y layout principal

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,5 @@
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-  <RouterView/>
+  <router-view></router-view>
 </template>
 
 <script setup lang="ts">
---
+++ src/modules/shop/layouts/ShopLayout.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+
+import TopMenu from "@/modules/shop/components/TopMenu.vue";
+import CustomFooter from "@/modules/shop/components/CustomFooter.vue";
+
+</script>
+
+<template>
+  <!-- Header Navbar -->
+  <top-menu></top-menu>
+
+
+
+
+  <!-- Products -->
+  <router-view></router-view>
+
+  <!-- Footer -->
+<custom-footer></custom-footer>
+
+
+</template>
+
+<style scoped>
+
+</style>
